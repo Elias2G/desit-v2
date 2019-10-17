6917d857a8f79a67d1b5f648403963fefcfbe69e
XImOnTRiX <domenik.wenghofer@gmail.com>
2019-10-17 14:18:21 +0200

    changed c directory

--- src/config/apiSettings.js
+++ src/config/apiSettings.js
@@ -2,7 +2,7 @@ export const masterkey = '3037aea2755689b197042ead2f012c';
 
 
 // export const ROOT_URL = 'http://127.0.0.1/cockpit-desit';
-export const ROOT_URL = `${window.location.origin}`
+export const ROOT_URL = `${window.location.origin}/cockpit-desit`;
 
 export const GET_COLLECTION = '/api/collections/get';
 export const GET_USER_API = '/api/cockpit/listUsers';
